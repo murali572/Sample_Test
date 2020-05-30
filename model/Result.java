62f5b3aecbf7bb5d11d751191219d79c2d1431ea
MURALI <murali572@gmail.com>
2020-05-30 15:20:35 +0530

    Added Results

---
+++ model/Result.java
@@ -0,0 +1,12 @@
+public class Result{
+    boolean success;
+    boolean  value;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
